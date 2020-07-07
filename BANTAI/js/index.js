2d447c3938e511b946546f0669e9747d1248c4bb
labiebhn <62648428+labiebhn@users.noreply.github.com>
2020-07-07 13:37:12 +0700

    Add files via upload

---
+++ BANTAI/js/index.js
@@ -0,0 +1,12 @@
+setTimeout(function() {
+    $('.loader_bg').fadeToggle();
+}, 1500);
+
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+});
+
+
